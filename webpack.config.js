736783d2810ccdd8d1a1ca1b38115d3fc0d3a03e
tearofenemy <calm.snow98@gmail.com>
2020-05-23 20:24:45 +0300

    final fix bug when appear err while compile js

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,7 @@ module.exports = {
         rules: [{
             test: /\.js$/,
             exclude: '/node_modules/',
-            use: {
-                loader: 'babel-loader'
-            }
+            loader: 'babel-loader'
         }]
     }
 };
