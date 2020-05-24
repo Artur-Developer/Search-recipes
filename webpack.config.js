9ee8e2ccffbce3ac49cb4a488242d61dc9dd9e00
tearofenemy <calm.snow98@gmail.com>
2020-05-24 23:38:38 +0300

    disable babel, create model and view for search, send test request for 'pizza' query. Return json object

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['babel-polyfill', './src/js/index.js'],
+    entry: ['./src/js/index.js'],
     output: {
         path: path.resolve(__dirname, 'frontend'),
         filename: 'js/bundle.js'
@@ -13,11 +13,12 @@ module.exports = {
             template: './src/index.html'
         })
     ],
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: '/node_modules/',
-            loader: 'babel-loader'
-        }]
-    }
+
+    // module: {
+    //     rules: [{
+    //         test: /\.js$/,
+    //         exclude: '/node_modules/',
+    //         loader: 'babel-loader'
+    //     }]
+    // }
 };
